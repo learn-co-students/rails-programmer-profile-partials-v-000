7bfb27cd9eae6902a3621bb6d4e2508befda3f64
Justin Thomas <jthomas0209@gmail.com>
2016-02-03 12:32:46 -0500

    Cleaned up views on attribute partial

--- app/models/programmer.rb
+++ app/models/programmer.rb
@@ -1,2 +1,9 @@
 class Programmer < ActiveRecord::Base
+  def attributes_for_show
+    self.instance_variables.select{|var| var == :home_country || var == :quote || var == :claim_to_fame}
+  end
+
+  def attribute_label(attribute)
+    attribute.to_s.split("_").join(" ").titlecase
+  end
 end

15c774643fa83cec02285e963a356d68ab8388fd
azheng249 <andy.zheng249@gmail.com>
2016-03-18 15:52:14 -0400

    Done.

--- app/models/programmer.rb
+++ app/models/programmer.rb
@@ -1,2 +1,8 @@
 class Programmer < ActiveRecord::Base
+
+  def programmer_info
+    ["home_country", "quote", "claim_to_fame"].collect do |attribute|
+      {attr: attribute, value: self[attribute]}
+    end
+  end
 end

154b4eb8aa9c6a463ec5aa7271393eea295d49e9
drywaterr24 <angelolea24@gmail.com>
2016-03-29 20:00:10 -0400

    Done.

--- app/models/programmer.rb
+++ app/models/programmer.rb
@@ -1,6 +1,8 @@
 class Programmer < ActiveRecord::Base
 
-
+    def attr_for_display
+      ['home_country', 'quote', 'claim_to_fame']
+    end
     def info(attr)
       "#{attr}".capitalize + ":"
     end

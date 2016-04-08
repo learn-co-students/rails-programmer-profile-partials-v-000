e950a95e5f62052cfabb07d2ca29b875bbf5f35b
Tom Johns <tommyskies@hotmail.com>
2016-04-08 16:23:22 -0600

    Done.

--- app/models/programmer.rb
+++ app/models/programmer.rb
@@ -1,2 +1,13 @@
 class Programmer < ActiveRecord::Base
+	def show_attributes
+		["home_country", "quote", "claim_to_fame"]
+	end
+	
+	
+	def my_attributes
+		show_attributes.each_with_object({}) do |att, hsh|
+			hsh[att] = attributes[att]
+		end
+		
+	end
 end

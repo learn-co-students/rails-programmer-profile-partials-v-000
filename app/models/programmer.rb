f551eab7fc8a4fa45b14a1e71a042cbea8493780
fiveinfinity <jlachance1@gmail.com>
2016-04-09 10:26:14 -0500

    Done.

--- app/models/programmer.rb
+++ app/models/programmer.rb
@@ -1,2 +1,17 @@
 class Programmer < ActiveRecord::Base
+  def programmer_attributes
+    ['home_country', 'quote', 'claim_to_fame']
+  end
+
+  def locals_hash
+    programmer_attributes.each_with_object({}) do |att, hash|
+      hash[att] = attributes[att]
+      # 
+      # hash = {
+      #   :home_country => {:attributes => 'home_country'},
+      #   :quote => {:attributes => 'quote'},
+      #   etc.
+      # }
+    end
+  end
 end

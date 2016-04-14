809eb631f3169276978a887b0b3d310ca7cce884
davdkm <davd.km@gmail.com>
2016-04-14 14:23:00 -0700

    Done.

--- app/models/programmer.rb
+++ app/models/programmer.rb
@@ -1,2 +1,16 @@
 class Programmer < ActiveRecord::Base
+
+  def attr_hash
+    # returns a hash of specific programmer attributes
+    # {
+    # :home_country => self.home_country,
+    # :quote => self.quote,
+    # :claim_to_fame => self.claim_to_fame
+    # }
+    programmer_attr.each_with_object({}) {|attr, hash| hash[attr] = attributes[attr]}
+  end
+
+  def programmer_attr
+    ['home_country', 'quote', 'claim_to_fame']
+  end
 end

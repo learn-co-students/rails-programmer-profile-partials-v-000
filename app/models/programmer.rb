48262963d17e0b50b8c7c1bd24d34135d5f90f0a
cernanb <cernanb@gmail.com>
2016-04-16 11:54:15 -0700

    rendered partials and layouts

--- app/models/programmer.rb
+++ app/models/programmer.rb
@@ -1,2 +1,11 @@
 class Programmer < ActiveRecord::Base
+  def attr_for_partial
+    ['home_country', 'quote', 'claim_to_fame']
+  end
+
+  def locals_hash
+    attr_for_partial.each_with_object({}) do |att, hash|
+      hash[att] = attributes[att]
+    end
+  end
 end

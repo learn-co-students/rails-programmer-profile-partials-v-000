deccce622cfa47adbd1fbcb250a0c9485ccc2d54
agdavid <antonio.g.david@gmail.com>
2016-04-22 11:03:03 -0400

    Done. Code solution to extra test and render attributes partial.

--- app/models/programmer.rb
+++ app/models/programmer.rb
@@ -1,11 +1,24 @@
 class Programmer < ActiveRecord::Base
+  
+  #create array of actual attributes to send to #locals_hash
   def attr_for_partial
-    ['home_country', 'quote', 'claim_to_fame']
+    ['quote', 'home_country', 'claim_to_fame']
   end
 
   def locals_hash
-    attr_for_partial.each_with_object({}) do |att, hash|
-      hash[att] = attributes[att]
+    #iterate over array of actual attributes
+    #use #each_with_object to create hash involving @programmer instance
+      #first variable is what you pass in from the array
+      #second variable is the resulting hash
+    attr_for_partial.each_with_object({}) do |attr, partial_hash|
+      #left side => sets key to actual attributes (e.g., claim_to_fame)
+      #right side => sets value to actual attributes value by accessing @programmer attributes hash
+      partial_hash[attr] = attributes[attr]
     end
+    # => returns hash of key-values
+      #{"quote"=>"A computer would deserve to be called intelligent if it could deceive a human into believing that it was human.", 
+      # "home_country"=>"United Kingdom", 
+      # "claim_to_fame"=>"The Turing machine, breaking the Enigma code"
+      #}
   end
 end

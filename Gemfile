c99dccbe05ea397be1e93b0b5a69727836b193eb
seanliu93 <sean93liu@gmail.com>
2016-05-05 15:01:34 -0400

    Done.

--- Gemfile
+++ Gemfile
@@ -24,7 +24,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 # Use Unicorn as the app server
 # gem 'unicorn'
-
+gem 'web-console', group: :development
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
@@ -33,7 +33,7 @@ group :development, :test do
   gem 'byebug'
 
   # Access an IRB console on exception pages or by using <%= console %> in views
-  gem 'web-console', '~> 2.0'
+
 
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
--- spec/views/partials_spec.rb
+++ spec/views/partials_spec.rb
@@ -37,18 +37,3 @@ describe "the programmers index page" do
     expect(programmers_index).to include("<%= render @programmers %>")
   end
 end
-
-# BONUS: Create a partial that renders a single attribute of a programmer
-xdescribe "the attribute partial" do
-  let(:programmer) { FactoryGirl.create(:programmer) }
-
-  it "renders any attribute of the programmer" do
-      view.lookup_context.prefixes = %w[programmers]
-      assign(:programmer, programmer)  
-      render :template => "programmers/show.html.erb"
-      expect(response).not_to include("programmer.send(attribute)")
-      expect(response).not_to include("programmer.home_country")
-    
-  end
-
-end

983031af435a4ee3272ef0bbdb07b0daced25207
jasonpaulso <hello@jasonpaulsouthwell.com>
2016-05-17 18:59:04 -0700

    Done.

--- app/models/programmer.rb
+++ app/models/programmer.rb
@@ -1,2 +1,8 @@
 class Programmer < ActiveRecord::Base
+
+  def public_attributes
+    excluded_attributes = ["id", "created_at", "updated_at", "image", "wikipedia_page"]
+    self.attributes.except(*excluded_attributes)
+  end
+
 end

b1079ca898fc7383556c2325b3011b7b1ce7f130
jasonpaulso <hello@jasonpaulsouthwell.com>
2016-05-17 19:17:49 -0700

    Done.

--- app/models/programmer.rb
+++ app/models/programmer.rb
@@ -1,8 +1,8 @@
 class Programmer < ActiveRecord::Base
+  include ActionView::Helpers::DateHelper
 
   def public_attributes
-    excluded_attributes = ["id", "created_at", "updated_at", "image", "wikipedia_page", "name"]
+    excluded_attributes = ["id", "created_at", "updated_at", "image", "wikipedia_page", "name", "birth_date", "death_date"]
     self.attributes.except(*excluded_attributes)
   end
-
 end

1c0dff076aa780491ec478b4e87d54bd358c92bb
Kevin Yongchul Kim <github email address>
2017-11-04 03:32:04 -0700

    Done.

--- app/models/programmer.rb
+++ app/models/programmer.rb
@@ -1,2 +1,9 @@
 class Programmer < ActiveRecord::Base
+  def attributes
+    ["home_country", "quote", "claim_to_fame"]
+  end
+
+  def title(attribute)
+    attribute.split("_").map {|word| word.capitalize}.join(" ")
+  end
 end

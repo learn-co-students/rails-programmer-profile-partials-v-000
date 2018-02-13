20a51f0cfc544198c443a3655b1c471a08a91b1f
mevanstron <michael@michaelgregoryevans.com>
2018-02-13 12:04:19 -0600

    Done.

--- app/models/programmer.rb
+++ app/models/programmer.rb
@@ -1,2 +1,9 @@
 class Programmer < ActiveRecord::Base
+
+  def attribute_to_label(attribute)
+    split_attribute = attribute.split('_').collect do |word|
+      word.capitalize
+    end
+    label = "#{split_attribute.join(" ")}:"
+  end
 end

c398db36ba2c3e86a9deed5335c3f7dec894296e
github username <github email address>
2018-02-19 12:06:39 -0800

    Done.

--- app/controllers/programmers_controller.rb
+++ app/controllers/programmers_controller.rb
@@ -1,9 +1,12 @@
+require 'pry'
 class ProgrammersController < ApplicationController
   def index
     @programmers = Programmer.all
   end
 
   def show
+    binding.pry
+    @hash = {home_country: "Home Country", quote: "Quote", claim_to_fame: "Claim To Fame"}
     @programmer = Programmer.find(params[:id])
   end
 end
